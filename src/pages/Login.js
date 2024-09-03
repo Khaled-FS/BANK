ac9bb04965112320e9819de72dadfe5c31834b8d
dalal <dalal.dyh@gmail.com>
2024-09-03 19:12:14 +0300

    login

---
+++ src/pages/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <div className="border flex ">
+        <h1>Login</h1>
+        <p>Username</p>
+        <p>Password</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
