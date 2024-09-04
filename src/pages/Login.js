ab1830509aed4aa75507abf1adb2f93bee48432f
dalal <dalal.dyh@gmail.com>
2024-09-04 19:01:43 +0300

    changes on login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,33 +1,68 @@
 import React from "react";
 import bank from "../assets/pngtree-debit-card-payment-png-image_5705181.jpeg";
-import Navbar from "../component/Navbar";
+import { useContext, useState } from "react";
+import { login } from "../api/auth";
+import { useMutation } from "react";
+import UserContext from "react";
 
 const Login = () => {
+  const [userInfo, setUserInfo] = useState({});
+
+  const [user, setUser] = useContext(UserContext);
+
+  const handleChange = (e) => {
+    setUserInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const { mutate: handleLogin } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: () => login(userInfo),
+    onSuccess: () => {
+      setUser(true);
+    },
+  });
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    handleLogin();
+  };
+
   return (
-    <>
-      <Navbar />
-      <div className="bg-gray-400 ">
-        <div className="border flex items-center justify-center h-96 space-x-40 h-dvh">
-          <div>
-            <img src={bank} alt="bank logo" />
-          </div>
+    <div className="bg-gray-400 ">
+      <div className="border flex items-center justify-center h-96 space-x-40 h-dvh">
+        <div>
+          <img className="" src={bank} alt="bank logo" />
+        </div>
+        <form onSubmit={handleFormSubmit}>
           <div>
             <h1>Login</h1>
             <div>
               <p>Username</p>
-              <input></input>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                onChange={handleChange}
+                required
+              />
             </div>
             <div>
               <p>Password</p>
-              <input></input>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                onChange={handleChange}
+                required
+              />
             </div>
             <div className="m-10">
               <button className="border ">login</button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
