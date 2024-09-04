496534b3683867b7eb657d94c1e0b513f35fa1dc
unknown <eng.Khaled10@outlook.com>
2024-09-04 05:42:06 +0300

    Navbar && api

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <button className="bg-white">sikje</button>
+            <button className="bg-white">nav</button>
           </div>
         </div>
       </div>
